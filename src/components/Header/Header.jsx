cc8dd41ff55141a1f918a19822c434e688692fa1
kolya <kybiyevych97@gmail.com>
2023-05-13 01:45:52 +0300

    TASK-3-12 --save points

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,34 @@
-import { HeaderContainer } from './Header.styled';
+
 import Navbar from '../Navbar/Navbar';
-import styled from './Headerstyle.module.scss'
-import { Layout, Space } from 'antd';
+import styled from './Header.module.scss'
+import { Button, Layout } from 'antd';
+import { Link } from 'react-router-dom';
+import {ReactComponent as ReactLogo} from '../assets/images/icon/logo.svg';
+import {ReactComponent as UserIcon} from '../assets/images/icon/userIcon.svg';
+import {ReactComponent as BurgerIcon} from '../assets/images/icon/menuHamburgerIcon.svg';
+
 
 const { Header } = Layout;
 
 const HeaderPage = () => {
   return (
-    <Header className={}>
-    <Navbar/>
-
-
+    <Header className={styled.header}>
+      <div className={styled.headerWrapper}>
+        <nav className={ styled.navigationBlock}>
+          <Link to={'/main'} className={styled.logoWrapper}>
+            <ReactLogo className ={styled.logo}/>
+          </Link>
+          <Navbar />
+        </nav>
+        <div className={styled.rightNavigationalBlock}>
+          <Button className={styled.userIconBtn}>
+            <UserIcon/>
+          </Button>
+          <Button  className={styled.menuBtn}>
+            <BurgerIcon/>
+          </Button>
+        </div>
+      </div>
 
     </Header>
 
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,12 +1,13 @@
 import { NavLink } from 'react-router-dom';
+import styled from './Navbar.module.scss'
 
-const Navbar = ({ children }) => {
+const Navbar = () => {
   return (
-   <>
+   <div className={styled.navbarWrapper}>
      <NavLink to="/news" end>News</NavLink>
      <NavLink to="/notices">Find pet</NavLink>
      <NavLink to="/friends">Our friends</NavLink>
-   </>
+   </div>
 
 
   );
