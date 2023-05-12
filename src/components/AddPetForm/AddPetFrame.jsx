ff96828b7ca91dfce82c8890541cf12c543af563
VladimirKoshovec <koshavladmir@gmail.com>
2023-05-12 16:42:30 +0300

    test_Vkoshovec

---
+++ src/components/AddPetForm/AddPetFrame.jsx
@@ -0,0 +1,11 @@
+import { Frame } from '../AddPetForm/addPetFrame.styled';
+import AddPetForm from './AddPetForm';
+
+const AddPetFrame = () => {
+    return (
+        <Frame>
+        </Frame>
+    );
+};
+
+export default AddPetFrame;
