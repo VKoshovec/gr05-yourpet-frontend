cbcebd076ae9db0b5b23d88065065ef0341d2944
kolya <kybiyevych97@gmail.com>
2023-05-12 23:27:32 +0300

    TASK-3-12 -add sccs; --add media query; -- add global variable scss

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,11 +1,11 @@
 import { HeaderContainer } from './Header.styled';
 import Navbar from '../Navbar/Navbar';
-
+import styled from './Headerstyle.module.scss'
 
 const Header = () => {
   return (
     <HeaderContainer>
-    <h3>Logo</h3>
+    <h3 className={styled.header}>Logo</h3>
     <Navbar/>
     <h3>User menu</h3>
     </HeaderContainer>
