71d1870599e21e323d0f94cf5e19034a59934d19
OlhaNekrylova <nekrylovaurist@gmail.com>
2023-05-14 21:00:53 +0300

    added buttons in NoticeCategoryItem

--- src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
+++ src/components/NoticeCategoryItem/NoticeCategoryItem.jsx
@@ -1,13 +1,41 @@
-import css from './NoticeCategoryItem.module.css';
-		
+import { useDispatch } from "react-redux";
+import React, { useEffect } from 'react';
+import { getNotices, fetchAddFavorite, fetchDeleteFavorite  } from "../../redux/data/operations";
+
+import css from './NoticeCategoryItem.module.scss';
 
 const NoticeCategoryItem = ({id, category, image, location, date, sex}) => {
+	const dispatch = useDispatch();
+
+    const noticeFavorite = {id, category, image, location, date, sex}
+
+    useEffect(()=> {
+        dispatch(getNotices());
+    }, [dispatch])
+
+    const addAndDeleteFavorite = () => {
+        if(!noticeFavorite) {
+            dispatch(fetchAddFavorite(category, image, location, date, sex));
+        }
+
+        dispatch(fetchDeleteFavorite(id));
+    }
+
+    // const openModal = () => {
+    //     dispatch(Modal);
+    // }
+	
 	return (
 	<li className={css.item}>
 		<div className={css.item}>{category}</div>
 		<button 
-            className={css.item}>icon</button>
-		<img className={css.item} src={image} alt="User avatar" width="48" />
+            className={css.item}
+            type="button"
+            onClick={() => addAndDeleteFavorite()}
+		>
+            icon
+        </button>
+        <img className={css.item} src={image} alt="Your pet" width="280" />
 		<div className={css.item}>icon
 		{location}</div>
 		<div className={css.item} >icon
@@ -15,10 +43,14 @@ const NoticeCategoryItem = ({id, category, image, location, date, sex}) => {
 		<div className={css.item}>icon
 		{sex}</div>
 		<h3 className={css.item}>Сute dog looking for a home</h3>
-		<button 
-        className={css.item}>LearnMore</button>
+		<button             
+            className={css.button}
+            type="button"
+            // onClickModal={() => openModal()}
+		>
+            LearnMore</button>
 		</li>
-		);
-		};
+	);
+};
 
-        export default NoticeCategoryItem;
+export default NoticeCategoryItem;
