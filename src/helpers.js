e0ce5d17bb8a62eaf1cf27b89140c70b9520ee13
Viktoriia Chumak <vikaff17@gmail.com>
2023-05-15 15:24:39 +0300

    created Register Page

--- src/helpers.js
+++ src/helpers.js
@@ -1,14 +1,36 @@
 export const validationFormLogin = ({ email, password }) => {
   const errors = {};
   if (!email) {
-    errors.email = 'Email is required';
+    errors.email = 'Email is required.';
   } else if (!/\S+@\S+\.\S+/.test(email)) {
-    errors.email = 'Email is invalid';
+    errors.email = 'Email is invalid.';
   }
   if (!password) {
-    errors.password = 'Password is required';
+    errors.password = 'Password is required.';
   } else if (password.length < 6) {
-    errors.password = 'Password must be at least 6 characters long';
+    errors.password = 'Password must be at least 6 characters long.';
+  }
+  return errors;
+};
+
+export const validationAuthForm = ({ email, password, confirm_password }) => {
+  const errors = {};
+  if (!email) {
+    errors.email = 'Email is required.';
+  } else if (!/\S+@\S+\.\S+/.test(email)) {
+    errors.email = 'Email is invalid.';
+  }
+
+  if (!password) {
+    errors.password = 'Password is required.';
+  } else if (password.length < 6) {
+    errors.password = 'Password must be at least 6 characters long.';
+  }
+
+  if (!confirm_password) {
+    errors.confirm_password = 'You must confirm your password.';
+  } else if (confirm_password !== password) {
+    errors.confirm_password = 'Passwords are different.';
   }
   return errors;
 };
