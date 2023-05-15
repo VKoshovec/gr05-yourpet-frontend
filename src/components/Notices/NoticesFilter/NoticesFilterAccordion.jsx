a679659d49de1b78f1f2a28c8b27e70609593e91
kolya <kybiyevych97@gmail.com>
2023-05-15 22:41:29 +0300

    TASK_26-31: --fix style btn; fix Notices navigate

--- src/components/Notices/NoticesFilter/NoticesFilterAccordion.jsx
+++ src/components/Notices/NoticesFilter/NoticesFilterAccordion.jsx
@@ -1,11 +1,48 @@
-import CustomButton from '../../CustomButton/CustomButton';
+import { Checkbox, Collapse } from 'antd';
+import { ReactComponent as ChevronIcon } from '../../assets/images/icon/chevron-down.svg';
+import cn from 'classnames';
+import styled from './NoticesFilter.module.scss';
+
+const { Panel } = Collapse;
+
 
 const NoticesFilterAccordion = () => {
+  // const { token } = theme.useToken();
+  const panelStyle = {
+    marginBottom: 24,
+    // background: token.colorFillAlter,
+    // borderRadius: token.borderRadiusLG,
+    border: 'none',
+  };
+
+
+    const onChange = (e) => {
+      console.log(`checked = ${e.target.checked}`);
+    };
+
 
 
   return (<>
+  <p>Filters</p>
+    <Collapse
+      bordered={false}
+      // defaultActiveKey={['-1']}
+      expandIcon={({ isActive }) => <ChevronIcon className={cn(styled.chevronIcon, { [styled.active] : isActive })} />}
+      // style={{
+      //   background: token.colorBgContainer,
+      // }}
+      className={styled.filterWrapper}
+    >
+      <Panel header="By age" key="1" className={styled.title}>
+        <Checkbox onChange={onChange}>Checkbox</Checkbox>
+      </Panel>
+      <Panel header="By gender" key="2" className={styled.title}>
 
-  </>);
+      </Panel>
+
+    </Collapse>
+    </>
+  );
 };
 
 export default NoticesFilterAccordion;
--- src/components/Notices/NoticesFilter/NoticesTrigger.jsx
+++ src/components/Notices/NoticesFilter/NoticesTrigger.jsx
@@ -1,13 +1,65 @@
-import CustomButton from '../../CustomButton/CustomButton';
+import { Button, Checkbox, Dropdown } from 'antd';
+import cn from 'classnames';
+import styled from './NoticesFilter.module.scss'
+import NoticesFilterAccordion from './NoticesFilterAccordion';
 
-const NoticesFilterTrigger = () => {
 
 
-  return (<>
-    <CustomButton type={'outlined'}>
-      Filter
-    </CustomButton>
-  </>);
-};
 
-export default NoticesFilterTrigger;
+
+
+const NoticesFilter = () => {
+
+
+  const onChange = (e) => {
+    console.log(`checked = ${e.target.checked}`);
+  };
+
+  // const items = [
+  //   {
+  //     key: '1',
+  //     label: (
+  //       <Checkbox onChange={onChange}>Checkbox</Checkbox>
+  //     ),
+  //   },
+  //   {
+  //     key: '2',
+  //     label: (
+  //       <a target="_blank" rel="noopener noreferrer" href="https://www.aliyun.com">
+  //         2nd menu item
+  //       </a>
+  //     ),
+  //   },
+  //   {
+  //     key: '3',
+  //     label: (
+  //       <a target="_blank" rel="noopener noreferrer" href="https://www.luohanacademy.com">
+  //         3rd menu item
+  //       </a>
+  //     ),
+  //   },
+  // ];
+
+  const accordionComponent = () => {
+
+    return  <NoticesFilterAccordion/>
+  }
+
+
+
+  return ( <>
+      <Dropdown
+        trigger={['click']}
+        // menu={{
+        //   items,
+        // }}
+        dropdownRender={accordionComponent}
+        placement="bottom"
+      >
+        <Button className={cn(styled.filterButton, 'outlined')}>Filter</Button>
+      </Dropdown>
+    </>
+  )
+}
+
+export default NoticesFilter
