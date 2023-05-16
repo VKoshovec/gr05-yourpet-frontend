c16a66b8cde1f7af3dc9cada45fb45eb4b8fb5c8
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-05-16 11:23:43 +0300

    fix userPage bud

--- src/pages/User/UserPage.jsx
+++ src/pages/User/UserPage.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import UserData from 'components/UserData/UserData';
 
 import Container from 'components/Container/Container';
-import Section from 'components/Section/Container';
+import Section from 'components/Section/Section';
 // import styles from './UserPage.module.css';
 
 const UserPage = () => {
