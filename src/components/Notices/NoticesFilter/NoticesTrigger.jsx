a990492b76649bc96c521e9391c9fcce5bb5a8cd
kolya <kybiyevych97@gmail.com>
2023-05-16 19:06:43 +0300

    TASK_30: fix styled filter button;

--- src/components/Notices/NoticesFilter/NoticesTrigger.jsx
+++ src/components/Notices/NoticesFilter/NoticesTrigger.jsx
@@ -3,6 +3,7 @@ import cn from 'classnames';
 import styled from './NoticesFilter.module.scss'
 import NoticesFilterAccordion from './NoticesFilterAccordion';
 import { useState } from 'react';
+import {ReactComponent as FilterBtnIcon} from 'components/assets/images/icon/filters-3.svg';
 
 
 const accordionComponent = () => {
@@ -20,11 +21,12 @@ const [isOpenFilter, setIsOpenFilter] = useState(false)
       <Dropdown
         trigger={['click']}
         dropdownRender={accordionComponent}
-        placement="bottom"
+        placement="bottomLeft"
         onOpenChange={()=>setIsOpenFilter(!isOpenFilter)}
 
+
       >
-        <Button className={cn(styled.filterButton, 'outlined', {[styled.isActive] : isOpenFilter})}>Filter</Button>
+        <Button className={cn(styled.filterButton, 'outlined', {[styled.isActive] : isOpenFilter})}><span className={styled.textBtn}>Filter</span><FilterBtnIcon/></Button>
       </Dropdown>
     </>
   )
