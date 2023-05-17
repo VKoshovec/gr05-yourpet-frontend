e212bf9997eae20ecd3cc3a52b677ca7a584067f
dmitrij-pularija <dmitrij.pularija@gmail.com>
2023-05-17 14:25:14 +0300

    bug fixed

--- src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
+++ src/components/NoticesCategoriesList/NoticesCategoriesList.jsx
@@ -3,7 +3,7 @@ import React, { useEffect } from 'react';
 import { getNoticesCategory } from "../../redux/data/operations";
 
 import NoticeCategoryItem from '../NoticeCategoryItem/NoticeCategoryItem';
-import css from './NoticesCategoriesList.module.scss';
+import css from './NoticesCategorieslist.module.scss';
 import {noticies} from '../../noticies';
 const NoticesCategoriesList = ({ onClick, onClickModal}) => {
     const dispatch = useDispatch();
