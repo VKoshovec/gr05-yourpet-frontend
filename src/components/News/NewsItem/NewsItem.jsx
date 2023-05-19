9daf0b52c615f10c82ef41228b78dc9d7127b739
Oleh Hubskiy <gubskyolegod@gmail.com>
2023-05-19 18:02:47 +0300

    Делает и выводит новости на странице news

--- src/components/News/NewsItem/NewsItem.jsx
+++ src/components/News/NewsItem/NewsItem.jsx
@@ -1,19 +1,35 @@
 import styles from './NewsItem.module.scss';
 
+import formatDate from '../formatDate';
+
 const NewsItem = ({ items }) => {
   const element = items.map(({ _id, title, text, imgUrl, date, url, id }) => (
-    <li key={_id} className="itemList">
-      <div className="line">
-        <div className="thumb">
-          <img src={imgUrl} alt={title} width={250} />
+    <li key={_id} className={styles.listItem}>
+      <div className={styles.line}></div>
+      <div className={styles.wrapper}>
+        <div className={styles.thumb}>
+          <div
+            className={styles.img}
+            style={{
+              background: `linear-gradient(0deg, rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.2)),
+    url(${imgUrl})`,
+            }}
+          ></div>
         </div>
-        <div className="content">
-          <h2 className="titleNews">{title}</h2>
-          <div className="itemtext">{text}</div>
-          <div className="itemFooter">
-            <span className="itemDate">{date}</span>
-            <a href={url} className="linkReadMore">
-              Read More
+        <div className={styles.content}>
+          <h2 className={styles.titleNews} title={title}>
+            {title}
+          </h2>
+          <p className={styles.itemText}>{text}</p>
+          <div className={styles.itemFooter}>
+            <span className={styles.itemDate}>{formatDate(date)}</span>
+            <a
+              href={url}
+              target="_blank"
+              className={styles.linkReadMore}
+              rel="noreferrer"
+            >
+              Read more
             </a>
           </div>
         </div>
@@ -22,7 +38,7 @@ const NewsItem = ({ items }) => {
   ));
   return (
     <div className="wrapper">
-      <ul className="listNews">{element}</ul>
+      <ul className={styles.listNews}>{element}</ul>
     </div>
   );
 };
