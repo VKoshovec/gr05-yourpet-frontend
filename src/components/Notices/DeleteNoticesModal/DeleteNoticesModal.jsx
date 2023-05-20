33f7ef74c933aed4cf1748ed98f2f85bf2e3d4c7
kolya <kybiyevych97@gmail.com>
2023-05-20 10:11:57 +0300

    fix notices slice; ---update delete modal

--- src/components/Notices/DeleteNoticesModal/DeleteNoticesModal.jsx
+++ src/components/Notices/DeleteNoticesModal/DeleteNoticesModal.jsx
@@ -1,15 +1,8 @@
 import React from 'react';
-import { ReactComponent as HeartIcon } from '../../assets/images/icon/heart.svg';
-
-import styled from './DeleteNoticesModal.module.scss';
 import CustomButton from '../../CustomButton/CustomButton';
-import { logDOM } from '@testing-library/react';
 
 const DeleteNoticesModal = ({data, onCancel}) => {
 
-  // console.log(data);
-  // const { id, category, image,  title, name, birthday, breed, location, sex, email, phone,
-  //   comments, addAndDeleteFavorite } = data;
 
     return(<><h4>Delete Notices Modal</h4>
         <CustomButton onClick={onCancel} type={'outlined'}>Cancel</CustomButton>
