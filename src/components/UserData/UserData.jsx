2dd51babe5832132c95221b9167353d329b880bf
Alex <ais_alex@ukr.net>
2023-05-21 04:11:11 +0300

    added user avatar change , fix scss styles

--- src/components/UserData/UserData.jsx
+++ src/components/UserData/UserData.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { Formik, Form, ErrorMessage } from 'formik';
 
@@ -8,6 +8,8 @@ import { Modal } from 'components/Modal/Modal';
 import { userPhotoValidationSchema } from './UserDataValidation';
 import PreviewImage from './PreviewImage/PreviewImage';
 import UserDataItem from './UserDataItem/UserDataItem';
+import { updateAvatar } from '../../redux/auth/operations';
+import { getAvatarUrl } from 'redux/auth/selectors';
 
 import { useAuth } from 'services/hooks';
 
@@ -29,6 +31,9 @@ const UserData = () => {
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const location = useLocation();
+  const avatarImg = useSelector(getAvatarUrl);
+
+  console.log(avatarImg);
 
   useEffect(() => {
     if (location.state === '/register') {
@@ -37,9 +42,6 @@ const UserData = () => {
       location.state = null;
     }
   }, []);
-
-  console.log(location);
-  console.log(fromPage);
 
   const initialValues = {
     image: null,
@@ -54,7 +56,9 @@ const UserData = () => {
     if (value.image.size === 0) {
       return;
     }
-    console.log(value);
+
+    const { image: avatar } = value;
+    dispatch(updateAvatar({ avatar }));
   };
 
   const hanleLogOut = () => {
@@ -64,79 +68,81 @@ const UserData = () => {
 
   return (
     <div className={styles.user_page}>
-      <h2 className={styles.user_form_title}>My information:</h2>
-      <div className={styles.user_form}>
-        <Formik
-          initialValues={initialValues}
-          validationSchema={userPhotoValidationSchema}
-          onSubmit={(values, actions) => {
-            console.log('submit', values);
+      <div className={styles.user_page_item}>
+        <h2 className={styles.user_form_title}>My information:</h2>
+        <div className={styles.user_form}>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={userPhotoValidationSchema}
+            onSubmit={(values, actions) => {
+              // console.log('submit', values);
 
-            setBtnChange(false);
-            handleChangeAvatar(values);
-          }}
-        >
-          {({ values, setFieldValue }) => (
-            <Form>
-              <div className={styles.user_input_wrapper}>
-                <div className={styles.user_input}>
-                  {values.image ? (
-                    <PreviewImage image={values.image} />
+              setBtnChange(false);
+              handleChangeAvatar(values);
+            }}
+          >
+            {({ values, setFieldValue }) => (
+              <Form>
+                <div className={styles.user_input_wrapper}>
+                  <div className={styles.user_input}>
+                    {values.image ? (
+                      <PreviewImage image={values.image} />
+                    ) : (
+                      <img
+                        src={avatarImg}
+                        alt="Default"
+                        width="182px"
+                        height="182px"
+                      />
+                    )}
+                  </div>
+
+                  <input
+                    id="#$%^&file"
+                    className={styles.user_inputfile}
+                    ref={fileRef}
+                    type="file"
+                    name="image"
+                    hidden
+                    onChange={event => {
+                      setFieldValue('image', event.target.files[0]);
+
+                      setBtnChange(true);
+                    }}
+                  />
+                  <ErrorMessage name="image" />
+
+                  {btnChange ? (
+                    <button
+                      htmlFor="#$%^&file"
+                      type="submit"
+                      className={styles.button}
+                    >
+                      <Confirm />
+                      Confirm
+                    </button>
                   ) : (
-                    <img
-                      src={user.avatarURL}
-                      alt="Default"
-                      width="182px"
-                      height="182px"
-                    />
+                    <label htmlFor="#$%^&file" className={styles.user_label}>
+                      <EditPfoto />
+                      Edit photo
+                    </label>
                   )}
                 </div>
+              </Form>
+            )}
+          </Formik>
+          <div className={styles.input_container}>
+            <UserDataItem user={user} />
 
-                <input
-                  id="#$%^&file"
-                  className={styles.user_inputfile}
-                  ref={fileRef}
-                  type="file"
-                  name="image"
-                  hidden
-                  onChange={event => {
-                    setFieldValue('image', event.target.files[0]);
-
-                    setBtnChange(true);
-                  }}
-                />
-                <ErrorMessage name="image" />
-
-                {btnChange ? (
-                  <button
-                    htmlFor="#$%^&file"
-                    type="submit"
-                    className={styles.button}
-                  >
-                    <Confirm />
-                    Confirm
-                  </button>
-                ) : (
-                  <label htmlFor="#$%^&file" className={styles.user_label}>
-                    <EditPfoto />
-                    Edit photo
-                  </label>
-                )}
-              </div>
-            </Form>
-          )}
-        </Formik>
-        <div className={styles.input_container}>
-          <UserDataItem user={user} />
-
-          <button
-            type="button"
-            className={styles.logoutBtn}
-            onClick={toggleModal}
-          >
-            <LogOut />
-            Log Out
-          </button>
+            <button
+              type="button"
+              className={styles.logoutBtn}
+              onClick={toggleModal}
+            >
+              <LogOut />
+              Log Out
+            </button>
+          </div>
         </div>
       </div>
       {modalShow && (
@@ -178,28 +184,6 @@ const UserData = () => {
           )}
         </Modal>
       )}
-      {/* {fromPage && (
-        <Modal closeModal={toggleModal}>
-          <div className={styles.user_modal}>
-            <h2 className={styles.user_modal_text}>&&&&&&&&&&&&&&&&&&&&?</h2>
-            <div className={styles.user_modalBtn}>
-              <button
-                className={styles.user_modalBtn_cancel}
-                onClick={toggleModal}
-              >
-                Cancel
-              </button>
-              <button
-                className={styles.user_modalBtn_logout}
-                onClick={hanleLogOut}
-              >
-                Yes
-                <LogOutW />
-              </button>
-            </div>
-          </div>
-        </Modal>
-      )} */}
     </div>
   );
 };
